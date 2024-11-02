7f47dd0cbf8593384d22947ecf8f614584333ea0
slater19 <sachinaggarwal19@gmail.com>
2024-11-02 14:11:48 +0530

    my changes1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import Xspellcheck from './xspellcheck.js'
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+     <Xspellcheck/>
+     </div>
+  );     
 }
 
 export default App;
